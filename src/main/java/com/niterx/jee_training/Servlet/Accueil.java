c230a5a185394def9e1572d31beaf15688bebcd9
giornotheme <vtang@outlook.fr>
2022-05-14 22:57:01 +0200

    included footer jsp

---
+++ src/main/java/com/niterx/jee_training/Servlet/Accueil.java
@@ -0,0 +1,19 @@
+package com.niterx.jee_training.Servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "Accueil", value = "/Accueil")
+public class Accueil extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.getServletContext().getRequestDispatcher("/accueil.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/webapp/footer.jsp
@@ -0,0 +1,10 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: vtang
+  Date: 14/05/2022
+  Time: 18:25
+  To change this template use File | Settings | File Templates.
+--%>
+<h3>
+    This is a shared footer
+</h3>
